b66b226e6fd634bae98acca99a64cdb07cb94af5
Akshantjain <akshant.jain2@gmail.com>
2020-04-13 18:10:57 +0530

    skills icons update

---
+++ js/logochange.js
@@ -0,0 +1,160 @@
+// Java Logo
+function changejavain () {
+    let react = document.querySelector("#java");
+    react.classList.remove("devicon-java-plain");
+    react.classList.add("devicon-java-plain-wordmark");
+    react.classList.add("colored");
+}
+
+function changejavaout () {
+    let react = document.querySelector("#java");
+    react.classList.remove("devicon-java-plain-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-java-plain");
+}
+
+// CPP logo
+function changecppin () {
+    let react = document.querySelector("#cpp");
+    react.classList.remove("devicon-cplusplus-plain");
+    react.classList.add("devicon-cplusplus-line");
+    react.classList.add("colored");
+}
+
+function changecppout () {
+    let react = document.querySelector("#cpp");
+    react.classList.remove("devicon-cplusplus-line");
+    react.classList.remove("colored");
+    react.classList.add("devicon-cplusplus-plain");
+}
+
+// Python Logo
+function changepythonin () {
+    let react = document.querySelector("#python");
+    react.classList.remove("devicon-python-plain");
+    react.classList.add("devicon-python-plain-wordmark");
+    react.classList.add("colored");
+}
+
+function changepythonout () {
+    let react = document.querySelector("#python");
+    react.classList.remove("devicon-python-plain-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-python-plain");
+}
+
+// SQL Logo
+function changesqlin () {
+    let react = document.querySelector("#mysql");
+    react.classList.remove("devicon-mysql-plain");
+    react.classList.add("devicon-mysql-plain-wordmark");
+    // react.classList.add("colored");
+}
+
+function changesqlout () {
+    let react = document.querySelector("#mysql");
+    react.classList.remove("devicon-mysql-plain-wordmark");
+    // react.classList.remove("colored");
+    react.classList.add("devicon-mysql-plain");
+}
+
+// Android Logo
+function changeandroidin () {
+    let react = document.querySelector("#android");
+    react.classList.remove("devicon-android-plain");
+    react.classList.add("devicon-android-plain-wordmark");
+    react.classList.add("colored");
+}
+
+function changeandroidout () {
+    let react = document.querySelector("#android");
+    react.classList.remove("devicon-android-plain-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-android-plain");
+}
+
+// HTML Logo 
+function changehtmlin () {
+    let react = document.querySelector("#html");
+    react.classList.remove("devicon-html5-plain");
+    react.classList.add("devicon-html5-plain-wordmark");
+    react.classList.add("colored");
+}
+
+function changehtmlout () {
+    let react = document.querySelector("#html");
+    react.classList.remove("devicon-html5-plain-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-html5-plain");
+}
+
+// CSS Logo
+function changecssin() {
+    let react = document.querySelector("#css");
+    react.classList.remove("devicon-css3-plain");
+    react.classList.add("devicon-css3-plain-wordmark");
+    react.classList.add("colored");
+}
+
+function changecssout() {
+    let react = document.querySelector("#css");
+    react.classList.remove("devicon-css3-plain-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-css3-plain");
+}
+
+// Javascript Logo
+function changejscriptin() {
+    let react = document.querySelector("#jscript");
+    react.classList.add("colored");
+}
+
+function changejscriptout() {
+    let react = document.querySelector("#jscript");
+    react.classList.remove("colored");
+}
+
+// Bootstrap Logo
+function changebootin () {
+    let react = document.querySelector("#boot");
+    react.classList.remove("devicon-bootstrap-plain");
+    react.classList.add("devicon-bootstrap-plain-wordmark");
+    // react.classList.add("colored");
+}
+
+function changebootout () {
+    let react = document.querySelector("#boot");
+    react.classList.remove("devicon-bootstrap-plain-wordmark");
+    // react.classList.remove("colored");
+    react.classList.add("devicon-bootstrap-plain");
+}
+
+// Git logo
+function changegitlogoin () {
+    let react = document.querySelector("#gitlogo");
+    react.classList.remove("devicon-github-plain");
+    react.classList.add("devicon-github-plain-wordmark");
+    react.classList.add("colored");
+}
+
+function changegitlogoout () {
+    let react = document.querySelector("#gitlogo");
+    react.classList.remove("devicon-github-plain-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-github-plain");
+}
+
+// React Logo
+function changereactin() {
+    let react = document.querySelector("#react");
+    react.classList.remove("devicon-react-original");
+    react.classList.add("devicon-react-original-wordmark");
+    react.classList.add("colored");
+}
+
+function changereactout() {
+    let react = document.querySelector("#react");
+    react.classList.remove("devicon-react-original-wordmark");
+    react.classList.remove("colored");
+    react.classList.add("devicon-react-original");
+}
