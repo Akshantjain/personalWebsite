2fabd54e5c40812a303aa2e4280432844274f056
Akshantjain <akshant.jain2@gmail.com>
2020-04-22 20:22:12 +0530

    update

---
+++ js/profile.js
@@ -0,0 +1,67 @@
+let profile = {
+    name: {
+        first: "Akshant",
+        last: "Jain"
+    },
+    email: "akshantjain@yahoo.co.in",
+    phone: "111-111-1111",
+
+    skills: [
+
+    ],
+    projects : [
+        {
+            name: "",
+            description: "",
+            tech: "",
+            url: "",
+            repo: "",
+            comments: ""
+        },
+
+        {
+            name: "",
+            description: "",
+            tech: "",
+            url: "",
+            repo: "",
+            comments: ""
+        },
+
+        {
+            name: "",
+            description: "",
+            tech: "",
+            url: "",
+            repo: "",
+            comments: ""
+        },
+
+        {
+            name: "",
+            description: "",
+            tech: "",
+            url: "",
+            repo: "",
+            comments: ""
+        },
+
+        {
+            name: "",
+            description: "",
+            tech: "",
+            url: "",
+            repo: "",
+            comments: ""
+        },
+
+        {
+            name: "",
+            description: "",
+            tech: "",
+            url: "",
+            repo: "",
+            comments: ""
+        }
+    ]
+}
